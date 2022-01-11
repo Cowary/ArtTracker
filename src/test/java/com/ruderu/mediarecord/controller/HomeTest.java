4503487aa890046703453438d45a5b5507a54c0f
Cowary <cowary@gmail.com>
2022-01-12 00:33:13 +0300

    ver 1.0

---
+++ src/test/java/com/ruderu/mediarecord/controller/HomeTest.java
@@ -0,0 +1,34 @@
+package com.ruderu.mediarecord.controller;
+
+import com.ruderu.mediarecord.controller.HomeController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HomeTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void correctTest() throws Exception {
+        this.mockMvc.perform(get("/anime"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Employees")))
+        ;
+    }
+}
